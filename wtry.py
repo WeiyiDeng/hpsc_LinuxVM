e499c8d039526c53e613b3359aeb98bea93097e2
uwhpsc <uwhpsc@ubuntu.(none)>
2016-09-13 07:03:44 -0700

    modified function script

--- wtry.py
+++ wtry.py
@@ -2,7 +2,8 @@ from numpy import *       # import all necessary funs from numpy
 
 s = 1
 x = 4
-for i in range(3):
+for k in range(6):
+	print("before %s interations, s = %s" %(k+1,s))      # %s is the location to insert the content after %
 	s = 0.5*(s+x/s)
 	
-print(s)
+print("\nafter %s interations, s = %s") %(k+1,s)

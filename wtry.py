fe807ee3702515726c2a1fc8f247877862c47233
uwhpsc <uwhpsc@ubuntu.(none)>
2016-09-13 07:16:37 -0700

    add tolerance to stop automatically

--- wtry.py
+++ wtry.py
@@ -1,9 +1,16 @@
 from numpy import *       # import all necessary funs from numpy
 
-s = 1
-x = 4
-for k in range(6):
+s = 0.01                             # inital guess
+x = 4                   
+max_k = 100                      # maximum number of iterations
+tol = 1.e-14                         # tolerance set to 1*10^-14
+
+for k in range(max_k):
 	print("before %s interations, s = %s" %(k+1,s))      # %s is the location to insert the content after %
+	s_prev = s
 	s = 0.5*(s+x/s)
+	delta_s = abs(s-s_prev)
+	if delta_s < tol:
+		break
 	
 print("\nafter %s interations, s = %s") %(k+1,s)

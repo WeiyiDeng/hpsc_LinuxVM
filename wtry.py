b25ae50d0f17fe971bf2fa1e1c3975db9acd6171
uwhpsc <uwhpsc@ubuntu.(none)>
2016-09-13 07:44:54 -0700

    sqrt function

--- wtry.py
+++ wtry.py
@@ -6,7 +6,8 @@ max_k = 100                      # maximum number of iterations
 tol = 1.e-14                         # tolerance set to 1*10^-14
 
 for k in range(max_k):
-	print("before %s interations, s = %s" %(k+1,s))      # %s is the location to insert the content after %
+	print("before %s interations, s = %20.15f" %(k+1,s))      # %s is the location to insert the content after %
+	# can change format too; %20.15f is for printing 20 digits with 15 digits after decimal in fixed point notation 
 	s_prev = s
 	s = 0.5*(s+x/s)
 	delta_s = abs(s-s_prev)
